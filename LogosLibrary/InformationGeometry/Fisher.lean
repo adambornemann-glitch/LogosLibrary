78b77e0ff9ab6be0dd0e02e3ff67da9a7366b5c6
Auggie <adambornemann@tenkai-q.com>
2026-02-26 23:57:53 -0500

    Create Fisher.lean

---
+++ LogosLibrary/InformationGeometry/Fisher.lean
@@ -0,0 +1,5 @@
+import LogosLibrary.InformationGeometry.Fisher.FisherInformation
+import LogosLibrary.InformationGeometry.Fisher.StatisticalModel
+import LogosLibrary.InformationGeometry.Fisher.Score
+import LogosLibrary.InformationGeometry.Fisher.FisherMetric
+import LogosLibrary.InformationGeometry.Fisher.StatisticalManifold
