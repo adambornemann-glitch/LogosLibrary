aa7ddd0a333c7157d2cf1657da6fe8e74ceef9aa
Auggie <adambornemann@tenkai-q.com>
2026-03-11 09:04:38 -0400

    Delete LogosLibrary/InformationGeometry directory

--- LogosLibrary/InformationGeometry/CramerRao.lean
+++
@@ -1,9 +0,0 @@
-/-
-Copyright (c) 2026 Information Geometry Contributors. All rights reserved.
-Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Adam Bornemann & co.
--/
-import LogosLibrary.InformationGeometry.CramerRao.Basic
-import LogosLibrary.InformationGeometry.CramerRao.Cross
-import LogosLibrary.InformationGeometry.CramerRao.CauchySchwarz
-import LogosLibrary.InformationGeometry.CramerRao.Bound
