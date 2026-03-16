2a12a1c51facb19e86c1cec60d877970c0633ea5
August <adambornemann@tenkai-q.com>
2026-03-16 04:33:10 -0400

    Update SewingLemma.lean

--- LogosLibrary/StochasticCalculus/Stage_0/SewingLemma.lean
+++ LogosLibrary/StochasticCalculus/Stage_0/SewingLemma.lean
@@ -2,22 +2,8 @@
 Copyright (c) 2026 Logos Library Formalization Project. All rights reserved.
 Released under MIT license as described in the file LICENSE.
 Authors: Adam Bornemann & Doctor Professor Baron von Wobble-Bob
-Filename: StochasticCalculus/SewingLemma.lean
+Filename: StochasticCalculus/Stage_0/SewingLemma.lean
 -/
-import LogosLibrary.StochasticCalculus.Stage_0.SewingLemma.Defs
-import LogosLibrary.StochasticCalculus.Stage_0.SewingLemma.Condition
-import LogosLibrary.StochasticCalculus.Stage_0.SewingLemma.LayerOne.Map
-
-
-import LogosLibrary.StochasticCalculus.Stage_0.SewingLemma.LayerTwo.Decay
-import LogosLibrary.StochasticCalculus.Stage_0.SewingLemma.LayerTwo.RightPe
-import LogosLibrary.StochasticCalculus.Stage_0.SewingLemma.LayerTwo.RefiCo
-import LogosLibrary.StochasticCalculus.Stage_0.SewingLemma.LayerTwo.Map
--- import LogosLibrary.StochasticCalculus.Stage_0.SewingLemma.LayerTwo.Specialize
--- import LogosLibrary.StochasticCalculus.Stage_0.SewingLemma.LayerTwo.YoungInt
-
-import LogosLibrary.StochasticCalculus.Stage_0.SewingLemma.LayerThree.Partition
-import LogosLibrary.StochasticCalculus.Stage_0.SewingLemma.LayerThree.ContinuousControl
-import LogosLibrary.StochasticCalculus.Stage_0.SewingLemma.LayerThree.ThetaEnergy
-import LogosLibrary.StochasticCalculus.Stage_0.SewingLemma.LayerThree.DyadicPartition
-import LogosLibrary.StochasticCalculus.Stage_0.SewingLemma.LayerThree.Map
+import LogosLibrary.StochasticCalculus.Stage_0.SewingLemma.LayerOne
+import LogosLibrary.StochasticCalculus.Stage_0.SewingLemma.LayerTwo
+import LogosLibrary.StochasticCalculus.Stage_0.SewingLemma.LayerThree
