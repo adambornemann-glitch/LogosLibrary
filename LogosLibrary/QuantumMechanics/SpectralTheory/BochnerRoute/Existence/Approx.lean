f9d390091277905e5f53c7e22b198c5a9ed17da2
August <adambornemann@tenkai-q.com>
2026-03-20 03:20:53 -0400

    Bochner alt files

---
+++ LogosLibrary/QuantumMechanics/SpectralTheory/BochnerRoute/Existence/Approx.lean
@@ -0,0 +1,10 @@
+/-
+Copyright (c) 2026 Logos Library Formalization Project. All rights reserved.
+Released under MIT license as described in the file LICENSE.
+Authors: Adam Bornemann
+Filename: BochnerRoute/Existence/Approx.lean
+-/
+import LogosLibrary.QuantumMechanics.SpectralTheory.BochnerRoute.Existence.Approx.Defs
+import LogosLibrary.QuantumMechanics.SpectralTheory.BochnerRoute.Existence.Approx.Integral
+import LogosLibrary.QuantumMechanics.SpectralTheory.BochnerRoute.Existence.Approx.Riemann
+import LogosLibrary.QuantumMechanics.SpectralTheory.BochnerRoute.Existence.Approx.Density
